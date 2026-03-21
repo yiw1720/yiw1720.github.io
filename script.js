572a02be7de4704f6b84945a949f68bafad1158e
Yi Wei <yiwei5158@gmail.com>
2026-03-21 00:12:22 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const menuBtn = document.getElementById("menuBtn");
+    const navLinks = document.getElementById("navLinks");
+
+    menuBtn.addEventListener("click", () => {
+      navLinks.classList.toggle("show");
+    });
+
+
+
+
+
+
+// sticky navbar, search icon 
